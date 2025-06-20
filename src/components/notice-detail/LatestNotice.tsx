9f330f9c33b80c047e164a904ce7f7c8cb18b337
Dong-Han-Kim <dh32110@gmail.com>
2025-06-21 02:33:01 +0900

    feat: 공고 상세 페이지 ui 수정 및 버튼 수정

--- src/components/notice-detail/LatestNotice.tsx
+++ src/components/notice-detail/LatestNotice.tsx
@@ -42,15 +42,14 @@ const LatestNotice = ({ checkPoint }: { checkPoint?: string | null }) => {
           <h3>최근에 본 공고가 없습니다.</h3>
         </div>
       ) : (
-        <div className="grid grid-cols-2 md:flex flex-wrap justify-start gap-2 sm:gap-3.5">
+        <div className="flex flex-wrap justify-start gap-2  lg:gap-3.5">
           {latestArray.map((item: LatestData) => {
             if (item.noticeId === '') return;
-            return <NoticeCard info={item} key={item.shopId} />;
+            return <NoticeCard info={item} key={item.noticeId} />;
           })}
         </div>
       )}
     </>
   );
 };
-
 export default LatestNotice;
--- src/components/notice-detail/modal/NoticeModal.tsx
+++ src/components/notice-detail/modal/NoticeModal.tsx
@@ -6,7 +6,7 @@ import { createPortal } from 'react-dom';
 const NoticeModal = ({ children }: { children: ReactNode }) => {
   const body = document.body;
   return createPortal(
-    <div className="absolute top-0 left-0 w-screen h-full flex items-center justify-center bg-black/25 bg z-40">
+    <div className="fixed top-0 left-0 w-screen h-full flex items-center justify-center bg-black/25 bg z-40">
       {children}
     </div>,
     body,
