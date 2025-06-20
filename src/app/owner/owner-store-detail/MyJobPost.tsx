6fbda7c554b9afd0b1c337476727044d30015156
TopiaYu <raemo963@naver.com>
2025-06-20 20:15:34 +0900

    feat: 마감공고, 지난공고 UI 처리

--- src/app/owner/owner-store-detail/MyJobPost.tsx
+++ src/app/owner/owner-store-detail/MyJobPost.tsx
@@ -21,7 +21,7 @@ interface MyJobPostProps {
   originalHourlyPay?: number;
 }
 
-const default_shop_image_url = '/logo.png';
+const default_shop_image_url = '/clock-closed-icon.png';
 
 export default function MyJobPost({
   notice,
@@ -43,12 +43,18 @@ export default function MyJobPost({
     : 0; // 조건이 맞지 않으면 0%로 설정
   const wageComparisonText = `기존 시급보다 ${increaseRate.toFixed(0)}%`;
 
+  const isExpired = notice.closed;
+  const startsAtDate = new Date(notice.startsAt);
+  const now = new Date();
+  const isPassed = startsAtDate.getTime() < now.getTime();
+
   const handleViewDetail = () => {
-    console.log(notice.closed);
-    if (!notice.closed) {
+    if (isExpired || isPassed) {
+      alert('마감이나 지난 공고입니다.');
+      //아예 못가게 하기 로 해도 됨
       router.push(`/owner/job-detail/${shopId}/${notice.id}`);
     } else {
-      alert('마감된 공고입니다.');
+      router.push(`/owner/job-detail/${shopId}/${notice.id}`);
     }
   };
 
@@ -68,10 +74,14 @@ export default function MyJobPost({
 
   return (
     <div
-      className={`p-4 max-w-[312px] max-h-[349px] border border-gray-20 bg-white rounded-2xl ${notice.closed ? 'cursor-not-allowed' : 'cursor-pointer'} `}
+      className={`p-4 max-w-[312px] max-h-[349px] border border-gray-20 bg-white rounded-2xl cursor-pointer `}
       onClick={handleViewDetail}
     >
-      <div className="w-full max-w-[280px] h-40 max-h-[160px] border rounded-xl relative overflow-hidden">
+      <div
+        className={`w-full max-w-[280px] h-40 max-h-[160px] border border-gray-30 rounded-xl relative overflow-hidden
+                  ${isExpired ? '' : 'cursor-pointer hover:shadow-md'}
+      `}
+      >
         <Image
           src={shopImageUrl || default_shop_image_url}
           alt={'가게 이미지'}
@@ -79,40 +89,65 @@ export default function MyJobPost({
           objectFit="cover"
           className="rounded-lg"
         />
+        {(isExpired || isPassed) && (
+          <div className="absolute inset-0 h-full w-full flex justify-center items-center bg-black opacity-70 z-10">
+            <span className="text-gray-20 font-bold text-3xl z-20">
+              {isExpired ? '마감 완료' : '지난 공고'}
+            </span>
+          </div>
+        )}
       </div>
       <div>
-        <h3 className="text-black mt-4 font-bold text-lg">{shopName}</h3>
+        <h3
+          className={`${isExpired || isPassed ? 'text-gray-20' : 'text-black'} mt-4 font-bold text-lg`}
+        >
+          {shopName}
+        </h3>
 
         <div className="flex gap-1 mt-2">
           <span className="flex items-center">
             <Image
-              src="/clock-icon.png"
+              src={isExpired || isPassed ? '/clock-closed-icon.png' : `/clock-icon.png`}
               width={16}
               height={16}
               className="min-[375px]:w-5 min-[375px]:h-5"
               alt="time"
             />
           </span>
-          <p className="text-gray-50 text-base max-[374px]:text-sm">{displayTime}</p>
+          <p
+            className={`${isExpired || isPassed ? 'text-gray-20' : 'text-gray-50'} text-base max-[374px]:text-sm`}
+          >
+            {displayTime}
+          </p>
         </div>
         <div className="flex gap-1 mt-2">
           <span className="flex items-center">
             <Image
-              src="/location-icon.png"
+              src={isExpired || isPassed ? '/location-closed-icon.png' : `/location-icon.png`}
               width={16}
               height={16}
               className="min-[375px]:w-5 min-[375px]:h-5"
               alt="location"
             />
           </span>
-          <p className="text-gray-50 text-base max-[374px]:text-sm">{shopAddress}</p>
+          <p
+            className={`${isExpired || isPassed ? 'text-gray-20' : 'text-gray-50'} text-base max-[374px]:text-sm`}
+          >
+            {shopAddress}
+          </p>
         </div>
 
         <div className="flex justify-between items-center mt-3">
-          <h2 className="text-xl font-bold">{notice.hourlyPay.toLocaleString('ko-KR')}원</h2>
+          <h2
+            className={`${isExpired || isPassed ? 'text-gray-20' : 'text-base'} text-xl font-bold`}
+          >
+            {notice.hourlyPay.toLocaleString('ko-KR')}원
+          </h2>
           {/* 있으면 보여주기 */}
           {showWageComparison && (
-            <div className="rounded-4xl bg-orange px-3 py-2 flex gap-1">
+            <div
+              className={`rounded-4xl px-3 py-2 flex gap-1 ${isExpired || isPassed ? 'bg-gray-10' : ' bg-orange'}`}
+            >
               <p className="text-white text-sm">{wageComparisonText}</p>
               <Image
                 src="/arrow-up-bold.png"
